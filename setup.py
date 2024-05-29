5984ad379694f6fe6acbf0dc805fceee8388ce2a
Anthony Li <38049831+doccstat@users.noreply.github.com>
2024-05-29 14:52:54 -0500

    Use c99 to setup pyx

--- setup.py
+++ setup.py
@@ -4,13 +4,17 @@ import numpy as np
 
 extensions = [
     Extension("watermarking.transform.its_levenshtein", [
-              "watermarking/transform/its_levenshtein.pyx"]),
+              "watermarking/transform/its_levenshtein.pyx"],
+              extra_compile_args=['-std=c99']),
     Extension("watermarking.transform.transform_levenshtein", [
-              "watermarking/transform/transform_levenshtein.pyx"]),
+              "watermarking/transform/transform_levenshtein.pyx"],
+              extra_compile_args=['-std=c99']),
     Extension("watermarking.gumbel.gumbel_levenshtein", [
-              "watermarking/gumbel/gumbel_levenshtein.pyx"]),
+              "watermarking/gumbel/gumbel_levenshtein.pyx"],
+              extra_compile_args=['-std=c99']),
     Extension("watermarking.gumbel.ems_levenshtein", [
-              "watermarking/gumbel/ems_levenshtein.pyx"]),
+              "watermarking/gumbel/ems_levenshtein.pyx"],
+              extra_compile_args=['-std=c99']),
 ]
 
 setup(
