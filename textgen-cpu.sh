2915b0aa0c4b316714b48d079ecdea25bfad3321
Anthony Li <38049831+doccstat@users.noreply.github.com>
2024-07-30 19:16:49 -0700

    textgen shell script

---
+++ textgen-cpu.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#SBATCH --job-name=textgen-cpu
+#SBATCH --nodes=1
+#SBATCH --ntasks=128
+#SBATCH --ntasks-per-node=128
+#SBATCH --cpus-per-task=1
+
+#SBATCH --time=7-00:00:00
+#SBATCH --partition=long,xlong
+
+#SBATCH --mem=128GB
+#SBATCH --output=/home/anthony.li/out/textgen-cpu.%j.out
+#SBATCH --error=/home/anthony.li/out/textgen-cpu.%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=anthony.li@tamu.edu
+
+module purge
+module load JupyterLab/4.0.5-GCCcore-12.3.0
+
+cd /home/anthony.li/llm-watermark-cpd
+
+mkdir -p results
+mkdir -p log
+
+echo "Starting job with ID ${SLURM_JOB_ID} on ${SLURM_JOB_NODELIST}"
+echo $(which python)
+
+export PYTHONPATH=".":$PYTHONPATH
+export HF_HOME=/scratch/user/anthony.li/hf_cache
+
+n=10
+
+for method in gumbel transform; do
+  python textgen.py --save results/ml3-watermark500-$method.p --n $n --batch_size 25 --m 500 --model meta-llama/Meta-Llama-3-8B --seed 1 --T 500 --k 20 --method $method
+  python textgen.py --save results/ml3-watermark250-nowatermark250-$method.p --n $n --batch_size 25 --m 250 --model meta-llama/Meta-Llama-3-8B --seed 1 --T 500 --k 20 --method $method --insertion_blocks_start 250 --insertion_blocks_length 250
+  python textgen.py --save results/ml3-watermark200-nowatermark100-watermark200-$method.p --n $n --batch_size 25 --m 500 --model meta-llama/Meta-Llama-3-8B --seed 1 --T 500 --k 20 --method $method --substitution_blocks_start 200 --substitution_blocks_end 300
+  python textgen.py --save results/ml3-watermark100-nowatermark100-watermark100-nowatermark100-watermark100-$method.p --n $n --batch_size 25 --m 400 --model meta-llama/Meta-Llama-3-8B --seed 1 --T 500 --k 20 --method $method --substitution_blocks_start 100 --substitution_blocks_end 200 --insertion_blocks_start 300 --insertion_blocks_length 100
+done
--- textgen.sh
+++ textgen.sh
@@ -17,26 +17,27 @@
 
 module purge
 module load JupyterLab/4.0.5-GCCcore-12.3.0
-module load R/4.3.2-gfbf-2023a
 
 cd /home/anthony.li/llm-watermark-cpd
 
-python setup.py build_ext --inplace
-
 mkdir -p results
+mkdir -p log
 
 export PYTHONPATH=".":$PYTHONPATH
+export HF_HOME=/scratch/user/anthony.li/hf_cache
+
+n=1000
 
 for method in gumbel transform; do
-  python textgen.py --save results/opt-watermark500-$method.p --n 1000 --batch_size 25 --m 500 --model facebook/opt-1.3b --seed 1 --T 500 --k 20 --method $method
-  python textgen.py --save results/opt-watermark250-nowatermark250-$method.p --n 1000 --batch_size 25 --m 250 --model facebook/opt-1.3b --seed 1 --T 500 --k 20 --method $method --insertion_blocks_start 250 --insertion_blocks_length 250
-  python textgen.py --save results/opt-watermark200-nowatermark100-watermark200-$method.p --n 1000 --batch_size 25 --m 500 --model facebook/opt-1.3b --seed 1 --T 500 --k 20 --method $method --substitution_blocks_start 200 --substitution_blocks_end 300
-  python textgen.py --save results/opt-watermark100-nowatermark100-watermark100-nowatermark100-watermark100-$method.p --n 1000 --batch_size 25 --m 400 --model facebook/opt-1.3b --seed 1 --T 500 --k 20 --method $method --substitution_blocks_start 100 --substitution_blocks_end 200 --insertion_blocks_start 300 --insertion_blocks_length 100
+  python textgen.py --save results/opt-watermark500-$method.p --n $n --batch_size 25 --m 500 --model facebook/opt-1.3b --seed 1 --T 500 --k 20 --method $method
+  python textgen.py --save results/opt-watermark250-nowatermark250-$method.p --n $n --batch_size 25 --m 250 --model facebook/opt-1.3b --seed 1 --T 500 --k 20 --method $method --insertion_blocks_start 250 --insertion_blocks_length 250
+  python textgen.py --save results/opt-watermark200-nowatermark100-watermark200-$method.p --n $n --batch_size 25 --m 500 --model facebook/opt-1.3b --seed 1 --T 500 --k 20 --method $method --substitution_blocks_start 200 --substitution_blocks_end 300
+  python textgen.py --save results/opt-watermark100-nowatermark100-watermark100-nowatermark100-watermark100-$method.p --n $n --batch_size 25 --m 400 --model facebook/opt-1.3b --seed 1 --T 500 --k 20 --method $method --substitution_blocks_start 100 --substitution_blocks_end 200 --insertion_blocks_start 300 --insertion_blocks_length 100
 done
 
 for method in gumbel transform; do
-  python textgen.py --save results/gpt-watermark500-$method.p --n 1000 --batch_size 25 --m 500 --model openai-community/gpt2 --seed 1 --T 500 --k 20 --method $method
-  python textgen.py --save results/gpt-watermark250-nowatermark250-$method.p --n 1000 --batch_size 25 --m 250 --model openai-community/gpt2 --seed 1 --T 500 --k 20 --method $method --insertion_blocks_start 250 --insertion_blocks_length 250
-  python textgen.py --save results/gpt-watermark200-nowatermark100-watermark200-$method.p --n 1000 --batch_size 25 --m 500 --model openai-community/gpt2 --seed 1 --T 500 --k 20 --method $method --substitution_blocks_start 200 --substitution_blocks_end 300
-  python textgen.py --save results/gpt-watermark100-nowatermark100-watermark100-nowatermark100-watermark100-$method.p --n 1000 --batch_size 25 --m 400 --model openai-community/gpt2 --seed 1 --T 500 --k 20 --method $method --substitution_blocks_start 100 --substitution_blocks_end 200 --insertion_blocks_start 300 --insertion_blocks_length 100
+  python textgen.py --save results/gpt-watermark500-$method.p --n $n --batch_size 25 --m 500 --model openai-community/gpt2 --seed 1 --T 500 --k 20 --method $method
+  python textgen.py --save results/gpt-watermark250-nowatermark250-$method.p --n $n --batch_size 25 --m 250 --model openai-community/gpt2 --seed 1 --T 500 --k 20 --method $method --insertion_blocks_start 250 --insertion_blocks_length 250
+  python textgen.py --save results/gpt-watermark200-nowatermark100-watermark200-$method.p --n $n --batch_size 25 --m 500 --model openai-community/gpt2 --seed 1 --T 500 --k 20 --method $method --substitution_blocks_start 200 --substitution_blocks_end 300
+  python textgen.py --save results/gpt-watermark100-nowatermark100-watermark100-nowatermark100-watermark100-$method.p --n $n --batch_size 25 --m 400 --model openai-community/gpt2 --seed 1 --T 500 --k 20 --method $method --substitution_blocks_start 100 --substitution_blocks_end 200 --insertion_blocks_start 300 --insertion_blocks_length 100
 done
