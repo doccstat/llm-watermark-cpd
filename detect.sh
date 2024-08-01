441146decdffbe41e627f79742b09288414330f1
Anthony Li <38049831+doccstat@users.noreply.github.com>
2024-07-31 23:14:12 -0700

    time

--- detect.sh
+++ detect.sh
@@ -3,8 +3,8 @@
 #SBATCH --job-name=detect
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=0-02:00:00
-#SBATCH --partition=short,medium,long,xlong
+#SBATCH --time=1-00:00:00
+#SBATCH --partition=medium,long,xlong
 #SBATCH --mem-per-cpu=1GB
 #SBATCH --output=/home/anthony.li/out/detect.%A.%a.out
 #SBATCH --error=/home/anthony.li/out/detect.%A.%a.err
