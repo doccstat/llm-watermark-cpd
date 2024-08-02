09b38a973460c4621eb4e89de0c574de2387281f
Anthony Li <38049831+doccstat@users.noreply.github.com>
2024-08-01 21:00:37 -0700

    ablation

---
+++ ablation-block-helper.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export PYTHONPATH=".":$PYTHONPATH
+
+method=$1
+Tindex=$2
+cpts=$3
+k=$4
+fixed_i=$5
+n_runs=$6
+
+n=1000
+
+python detect.py --token_file "results/ml3-${cpts}changepoints-${method}.p" --n ${n} --model meta-llama/Meta-Llama-3-8B --seed 1 --Tindex ${Tindex} --k ${k} --method ${method} --fixed_i ${fixed_i} --n_runs ${n_runs}
