9f255d530ef995c004a4746eccc7d7ac8fe0dd70
Anthony Li <38049831+doccstat@users.noreply.github.com>
2024-10-18 22:11:41 -0500

    Update time limit and memory requirements.

--- 3-detect.sh
+++ 3-detect.sh
@@ -3,9 +3,9 @@
 #SBATCH --job-name=detect
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=0-12:00:00
+#SBATCH --time=1-00:00:00
 #SBATCH --partition=medium,long,xlong
-#SBATCH --mem-per-cpu=500MB
+#SBATCH --mem-per-cpu=400MB
 #SBATCH --output=/home/anthony.li/llm-watermark-cpd/log/detect.%A.%a.out
 #SBATCH --error=/home/anthony.li/llm-watermark-cpd/log/detect.%A.%a.err
 #SBATCH --mail-type=FAIL,TIME_LIMIT,END
