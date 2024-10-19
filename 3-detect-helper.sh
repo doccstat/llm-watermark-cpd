8a44914d280044b28dbb9d79a9bbf9032e49b34f
Anthony Li <38049831+doccstat@users.noreply.github.com>
2024-10-18 21:55:31 -0500

    Update detection commands.

--- 3-detect-helper.sh
+++ 3-detect-helper.sh
@@ -8,7 +8,7 @@ rm -f 3-detect-commands.sh
 
 for method in gumbel transform; do
   for cpts in 0 1 2 4 9 19; do
-    for model_prefix in ml3; do
+    for model_prefix in opt gpt ml3; do
       if [ "$model_prefix" = "opt" ]; then
         model="facebook/opt-1.3b"
       elif [ "$model_prefix" = "gpt" ]; then
@@ -21,7 +21,7 @@ for method in gumbel transform; do
         # rm -rf results/$model_prefix-$method-$watermark_key_length-$cpts-$rolling_window_size-$permutation_count-detect
         mkdir -p results/$model_prefix-$method-$watermark_key_length-$cpts-$rolling_window_size-$permutation_count-detect
 
-        for prompt_index in $(seq 0 9); do
+        for prompt_index in $(seq 0 499); do
           for rolling_window_index in $(seq 0 499); do
             echo "python detect.py --token_file results/$model_prefix-$method-$watermark_key_length-$cpts --model $model --method $method --watermark_key_length $watermark_key_length --rolling_window_size $rolling_window_size --permutation_count $permutation_count --seed $seed --Tindex $prompt_index --rolling_window_index $rolling_window_index" >> 3-detect-commands.sh
           done
